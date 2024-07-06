8a5e5dec6502be902bab8e3332c6a4bb132f8a76
deadlasertom <deadlasertom@gmail.com>
2024-07-06 22:50:14 +0300

    updated test_for_hw_15.py

--- lesson_15/test_for_hw_15.py
+++ lesson_15/test_for_hw_15.py
@@ -9,16 +9,25 @@ from homework_15 import log_event
 
 class TestLoginSystem(unittest.TestCase):
 
-    def setUp(self):
-        # Очищення лог-файлу перед кожним тестом
-        with open('login_system.log', 'w'):
-            pass
 
     def test_successful_login(self):
         log_event('john_doe', 'success')
         with open('login_system.log', 'r') as log_file:
             log_content = log_file.read()
             self.assertIn('Login event - Username: john_doe, Status: success', log_content)
+
+
+    def test_old_password(self):
+        log_event('jane_doe', 'expired')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: jane_doe, Status: expired', log_content)
+
+    def test_incorrect_password(self):
+        log_event('jack_doe', 'failed')
+        with open('login_system.log', 'r') as log_file:
+            log_content = log_file.read()
+            self.assertIn('Login event - Username: jack_doe, Status: failed', log_content)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
