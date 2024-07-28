43e5efb9fb27a2dce6920481b9830f656b0a4c3b
deadlasertom <deadlasertom@gmail.com>
2024-07-28 22:56:36 +0300

    added homework_22.py, updated hillel_api.py, removed irrelevant files

--- abous/ideas_for_test/heartbeat/hb_proces.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-filename = Path(__file__).parent / "hblog"
-print(filename)
-with open(filename, mode="r") as f:
-    lines = f.readline()
--- abous/ideas_for_test/hillel_test_api/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import requests
-
-
-@pytest.fixture(scope='session')
-def s():
-    """create Session"""
-    return requests.session()
